d65e9133aefc63f858dbd3f7766a3691cfc8d77b
Karunanidhi Sharma <Karunanidhisharma16@users.noreply.github.com>
2026-01-20 20:17:08 +0530

    vercel link changed

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Allow requests from your frontend domain
 app.use(cors({
-  origin: ['https://karunanidhi16.vercel.app', 'http://localhost:5173'],
+  origin: ['https://karunanidhii16.vercel.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
